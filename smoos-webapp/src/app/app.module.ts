d6c167869ab1b61314f8464f368f42060dc97e48
juliads15 <juliadsantiago1@gmail.com>
2021-04-21 17:24:45 -0300

    commit frontend julia

--- smoos-webapp/src/app/app.module.ts
+++ smoos-webapp/src/app/app.module.ts
@@ -4,15 +4,42 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterObraComponent } from './backoffice/components/register-obra/register-obra.component';
+import { IndexSmoosComponent } from './index-smoos/index-smoos.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MultiSelectModule} from 'node_modules/primeng/multiselect';
+import {PasswordModule} from 'node_modules/primeng/password';
+import { RegisterArtistComponent } from './backoffice/components/register-artist/register-artist.component';
+import { HeaderComponent } from './shared/header/header.component';
+import {CalendarModule} from 'primeng/calendar';
+import {RadioButtonModule} from 'primeng/radiobutton';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterObraComponent
+    RegisterObraComponent,
+    IndexSmoosComponent,
+    LoginPageComponent,
+    SignUpPageComponent,
+    RegisterArtistComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    PasswordModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MultiSelectModule,
+    CalendarModule, 
+    RadioButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ smoos-webapp/src/app/shared/enums/movie-genre.enum.ts
@@ -0,0 +1,13 @@
+enum MovieGenre{
+    action = 'action',
+    adventure = 'adventure',
+    fantasy =  'fantasy',
+    scifi = 'scifi',
+    drama = 'drama',
+    biography = 'biography',
+    romance = 'romance',
+    musical = 'muscal',
+    comedy = 'comedy',
+    horror = 'horror',
+    romcom = 'romcom'
+}
